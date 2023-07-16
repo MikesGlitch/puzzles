f5fc3a585943840964744022b9f1c6111ad28488
Michael Clark <mclarkgb@gmail.com>
2023-07-16 13:50:46 +0100

    start to algo project

---
+++ algorithms/BubbleSort.cs
@@ -0,0 +1,19 @@
+namespace algorithms;
+
+public class BubbleSort
+{
+    [Theory]
+    [InlineData(new int[] { 8, 3, 5, 6, 1, 4, 3 }, new int[] {1, 3, 3, 4, 5, 6, 8 } )]
+    public void Sort(int[] data, int[] expected)
+    {
+        for (var i = 0; i < data.Length; i++) {
+            for (var j = 0; j < data.Length -1 - i; j++) {
+                if (data[j] > data[j + 1]) {
+                    (data[j + 1], data[j]) = (data[j], data[j + 1]);
+                }
+            }
+        }
+
+        Assert.Equal(data, expected);
+    }
+}
