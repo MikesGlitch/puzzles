2a2d925f293acf3972ce2b77c5b750d439fec54e
Mike Clark <mclarkgb@gmail.com>
2023-07-20 21:15:52 +0100

    better

--- algorithms/BinarySearch.cs
+++ algorithms/BinarySearch.cs
@@ -12,6 +12,8 @@ public class BinarySearch
     // return whether or not item is found
     // Time Complexity: O(log n) - because of the halfing nature of it
     [Theory]
+    [InlineData(new int[] { 1, 2, 3 }, 0, false)]
+    [InlineData(new int[] { 1, 2, 3 }, 1, true)]
     [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 5, true)]
     [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 3, true)]
     [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 8, true)]
@@ -20,23 +22,19 @@ public class BinarySearch
     {
         bool found = false;
         var low = 0;
-        var high = data.Length;
+        var high = data.Length - 1;
         
         do {
             var midpoint = (int)Math.Floor((decimal)low + (high - low) / 2);
             if (searchValue == data[midpoint]) { 
                 found = true;
+                break;
             } else if (searchValue > data[midpoint]) {
                 low = midpoint + 1;
             } else {
-                high = midpoint;
+                high = midpoint - 1;
             }
-            
-            if (low == high) {
-                break;
-            }
-        } while(found == false);
-
+        } while(low <= high);
 
         Assert.Equal(found, expected);
     }
